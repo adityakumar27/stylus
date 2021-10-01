ea1226bccbb9076423a532e2b094291cfd0c61ee
adityakumar27 <kaditya061@gmail.com>
2021-10-01 16:57:27 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './Screen/HomeScreen';
+import ProductScreen from './Screen/ProductScreen';
 
 const App= () => {
   return (
-    <>
+    
+    <Router>
      <Header />
      <main className='py-4'>
        <Container>
-         <HomeScreen/>
+         <Route path='/' compoent={HomeScreen} exact />
+         <Route path='/product/:id' compoent={ProductScreen}  />
        </Container>
       </main>
      <Footer />
-    </>
+    </Router>
   )
 }
 
