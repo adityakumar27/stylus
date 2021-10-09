69e08936fb86dec92fd85799d0dcfcfbd8cb33e3
adityakumar27 <kaditya061@gmail.com>
2021-10-09 10:12:15 +0530

    commit

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <Container>
             <Row>
                 <Col className='text-center py-3'>
-                  Copyright &copy; Stylus Stationery  
+                  Copyright &copy; Stylu Stationery  
 
                 </Col>
             </Row>
