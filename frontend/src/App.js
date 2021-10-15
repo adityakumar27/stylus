e00c3a85681aac3d19ad676d65d460cf4a7182de
adityakumar27 <kaditya061@gmail.com>
2021-10-15 13:45:43 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './Screen/HomeScreen';
 import ProductScreen from './Screen/ProductScreen';
+import CartScreen from './Screen/CartScreen';
 
 const App= () => {
   return (
@@ -15,6 +16,7 @@ const App= () => {
        <Container>
          <Route path='/' component={HomeScreen} exact />
          <Route path='/product/:id' component={ProductScreen}  />
+         <Route path='/cart/:id?' component={CartScreen}  />
        </Container>
       </main>
      <Footer />
