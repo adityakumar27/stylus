dc34b9dd8af62ec4480342da3c4a9909a8122684
adityakumar27 <kaditya061@gmail.com>
2021-11-02 11:30:13 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,10 @@ import PaymentScreen from './Screen/PaymentScreen';
 import PlaceOrderScreen from './Screen/PlaceOrderScreen';
 import OrderScreen from './Screen/OrderScreen';
 import UserListScreen from './Screen/UserListScreen';
+import UserEditScreen from './Screen/UserEditScreen';
+import ProductListScreen from './Screen/ProductListScreen';
+import ProductEditScreen from './Screen/ProductEditScreen';
+import OrderListScreen from './Screen/OrderListScreen';
 
 const App= () => {
   return (
@@ -32,6 +36,10 @@ const App= () => {
        <Route path='/product/:id' component={ProductScreen}  />
        <Route path='/cart/:id?' component={CartScreen}  />
        <Route path='/admin/userlist' component={UserListScreen}  />
+       <Route path='/admin/user/:id/edit' component={UserEditScreen}  />
+       <Route path='/admin/productlist' component={ProductListScreen}  />
+       <Route path='/admin/orderlist' component={OrderListScreen}  />
+       <Route path='/admin/product/:id/edit' component={ProductEditScreen}  />
        <Route path='/' component={HomeScreen} exact />
        </Container>
       </main>
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -10,7 +10,10 @@ import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGOUT,USER_LOGIN_SUCCESS,
    USER_LIST_RESET,
    USER_DELETE_REQUEST,
    USER_DELETE_SUCCESS,
-   USER_DELETE_FAIL} from '../constants/userConstants'
+   USER_DELETE_FAIL,
+   USER_UPDATE_REQUEST,
+   USER_UPDATE_SUCCESS,
+   USER_UPDATE_FAIL} from '../constants/userConstants'
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 export const login =(email, password) => async (dispatch) => {
     try {
@@ -216,6 +219,41 @@ export const logout = () => (dispatch) => {
       dispatch({
         type: USER_DELETE_FAIL,
         payload: 
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      })
+    }
+  }
+
+  export const updateUser = (user) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: USER_UPDATE_REQUEST,
+      })
+  
+      const {
+        userLogin: { userInfo },
+      } = getState()
+  
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      }
+  
+      const { data } = await axios.put(`/api/users/${user._id}`, user, config)
+  
+      dispatch({ type: USER_UPDATE_SUCCESS})
+  
+      dispatch({ type: USER_DETAILS_SUCCESS, payload: data })
+  
+      dispatch({ type: USER_DETAILS_RESET })
+    } catch (error) {
+      dispatch({
+        type: USER_UPDATE_FAIL,
+        payload:
         error.response && error.response.data.message
           ? error.response.data.message
           : error.message
