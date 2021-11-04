9fbb7930dd577efe208d42183aab73af42028eac
adityakumar27 <kaditya061@gmail.com>
2021-11-04 11:08:08 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -49,8 +49,8 @@ if (process.env.NODE_ENV === 'production') {
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
-app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+app.listen(3000, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
 )
 )
